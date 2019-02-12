e1899d5d5251f2b44b96d55e21cc312c26c9f1f9
junhojohn7 <junhojohn@gmail.com>
2019-02-12 22:21:44 +0900

    implements detail page.

---
+++ app/src/main/java/demo/onstar/car/suresofttech/com/carremotecontroldemo/activities/DetailPageActivity.java
@@ -0,0 +1,20 @@
+package demo.onstar.car.suresofttech.com.carremotecontroldemo.activities;
+
+import demo.onstar.car.suresofttech.com.carremotecontroldemo.R;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class DetailPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detailpage);
+    }
+}
